f14013c1d2d8ce8fcf71067f5aaeba449bf8c824
Joker2770 <1214220480@qq.com>
2024-10-13 09:36:24 +0800

    refactor: use option<probe_rs_handler> instead :construction:

--- src/rtt_opts.rs
+++ src/rtt_opts.rs
@@ -22,12 +22,17 @@ pub mod m_rtt_opts {
     use eframe::egui;
     use egui_file::FileDialog;
     use probe_rs::rtt::ScanRegion;
-    use std::{borrow::Borrow, collections::VecDeque, path::PathBuf, time::Duration};
+    use std::{
+        borrow::{Borrow, BorrowMut},
+        collections::VecDeque,
+        path::PathBuf,
+        time::Duration,
+    };
 
     #[derive(Default)]
     pub struct RTTIO {
         probe_selected_idx: usize,
-        probe_rs_handler: ProbeRsHandler,
+        probe_rs_handler: Option<ProbeRsHandler>,
         target_chip_name: String,
         b_try_to_read: bool,
         cur_target_core_idx: usize,
@@ -44,83 +49,103 @@ pub mod m_rtt_opts {
     impl RTTIO {
         pub fn ui(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
             ui.horizontal(|ui| {
-                if 0 >= self.probe_rs_handler.probes_list.len() {
-                    self.probe_rs_handler.get_probes_list();
+                if let None = self.probe_rs_handler.borrow_mut() {
+                    self.probe_rs_handler = Some(ProbeRsHandler::default());
                 }
+                if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                    if 0 >= h.probes_list.len() {
+                        h.get_probes_list();
+                    }
+                }
+
                 egui::ComboBox::from_label("probe")
                     .selected_text(format!("{}", self.probe_selected_idx))
                     .show_ui(ui, |ui| {
-                        for (i, p) in self.probe_rs_handler.probes_list.iter().enumerate() {
-                            ui.selectable_value(
-                                &mut self.probe_selected_idx,
-                                i,
-                                format!(
-                                    "{} (pid: {} vid: {})",
-                                    p.identifier.as_str(),
-                                    p.product_id,
-                                    p.vendor_id
-                                ),
-                            );
+                        if let Some(h) = self.probe_rs_handler.borrow() {
+                            for (i, p) in h.probes_list.iter().enumerate() {
+                                ui.selectable_value(
+                                    &mut self.probe_selected_idx,
+                                    i,
+                                    format!(
+                                        "{} (pid: {} vid: {})",
+                                        p.identifier.as_str(),
+                                        p.product_id,
+                                        p.vendor_id
+                                    ),
+                                );
+                            }
                         }
                     });
                 if ui.button("refresh").clicked() {
-                    self.probe_rs_handler.get_probes_list();
+                    if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                        h.get_probes_list();
+                    }
                 }
             });
-            if 0 >= self.probe_rs_handler.chips_list.len() {
-                self.probe_rs_handler.get_availabe_chips();
+            if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                if 0 >= h.chips_list.len() {
+                    h.get_availabe_chips();
+                }
             }
 
             ui.horizontal(|ui| {
-                egui::ComboBox::from_label("target")
-                    .selected_text(format!("{}", self.target_chip_name))
-                    .show_ui(ui, |ui| {
-                        for t in self.probe_rs_handler.chips_list.iter() {
-                            ui.selectable_value(&mut self.target_chip_name, t.to_string(), t);
+                if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                    egui::ComboBox::from_label("target")
+                        .selected_text(format!("{}", self.target_chip_name))
+                        .show_ui(ui, |ui| {
+                            for t in h.chips_list.iter() {
+                                ui.selectable_value(&mut self.target_chip_name, t.to_string(), t);
+                            }
+                        });
+                    if ui.button("attach").clicked() {
+                        let _ = h.attach_target(self.probe_selected_idx, &self.target_chip_name);
+                        h.get_core_num();
+                    }
+                    if ui.button("attach under reset").clicked() {
+                        let _ = h.attach_target_under_reset(
+                            self.probe_selected_idx,
+                            &self.target_chip_name,
+                        );
+                        h.get_core_num();
+                    }
+                    if ui.button("reset all").clicked() {
+                        match h.reset_all_cores() {
+                            Ok(_) => {
+                                self.cur_target_core_idx = 0;
+                                self.cur_target_channel_idx = 0;
+                                self.b_try_to_read = false;
+                                self.target_chip_name = "".to_owned();
+                                self.b_get_scan_region = false;
+                                self.selected_file = None;
+                                self.probe_rs_handler = None;
+                            }
+                            Err(_) => {}
                         }
-                    });
-                if ui.button("attach").clicked() {
-                    let _ = self
-                        .probe_rs_handler
-                        .attach_target(self.probe_selected_idx, &self.target_chip_name);
-                    self.probe_rs_handler.get_core_num();
-                }
-                if ui.button("attach under reset").clicked() {
-                    let _ = self
-                        .probe_rs_handler
-                        .attach_target_under_reset(self.probe_selected_idx, &self.target_chip_name);
-                    self.probe_rs_handler.get_core_num();
-                }
-                if ui.button("reset all").clicked() {
-                    match self.probe_rs_handler.reset_all_cores() {
-                        Ok(_) => {
-                            self.cur_target_core_idx = 0;
-                            self.cur_target_channel_idx = 0;
-                            self.b_try_to_read = false;
-                            self.target_chip_name = "".to_owned();
-                            self.b_get_scan_region = false;
-                            self.selected_file = None;
-                        }
-                        Err(_) => {}
                     }
                 }
             });
 
             ui.horizontal(|ui| {
-                egui::ComboBox::from_label("core")
-                    .selected_text(format!("{}", self.cur_target_core_idx))
-                    .show_ui(ui, |ui| {
-                        for c in 0..self.probe_rs_handler.target_cores_num {
-                            ui.selectable_value(&mut self.cur_target_core_idx, c, format!("{}", c));
-                        }
-                    });
+                if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                    egui::ComboBox::from_label("core")
+                        .selected_text(format!("{}", self.cur_target_core_idx))
+                        .show_ui(ui, |ui| {
+                            for c in 0..h.target_cores_num {
+                                ui.selectable_value(
+                                    &mut self.cur_target_core_idx,
+                                    c,
+                                    format!("{}", c),
+                                );
+                            }
+                        });
 
-                if ui.button("attach rtt").clicked() {
-                    match self.probe_rs_handler.attach_rtt(self.cur_target_core_idx) {
-                        Ok(_) => {}
-                        Err(_) => {}
+                    if ui.button("attach rtt").clicked() {
+                        match h.attach_rtt(self.cur_target_core_idx) {
+                            Ok(_) => {}
+                            Err(_) => {}
+                        }
+                        h.get_up_channels_size();
                     }
-                    self.probe_rs_handler.get_up_channels_size();
                 }
             });
 
@@ -154,51 +179,52 @@ pub mod m_rtt_opts {
                     }
 
                     if ui.button("get scan region from elf").clicked() {
-                        match self
-                            .probe_rs_handler
-                            .get_scan_region(self.selected_file.borrow(), None)
-                        {
-                            Ok(_) => {
-                                if let Some(sr) = self.probe_rs_handler.scan_region.borrow() {
-                                    match sr {
-                                        ScanRegion::Exact(_) => {
-                                            self.b_get_scan_region = true;
+                        if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                            match h.get_scan_region(self.selected_file.borrow(), None) {
+                                Ok(_) => {
+                                    if let Some(sr) = h.scan_region.borrow() {
+                                        match sr {
+                                            ScanRegion::Exact(_) => {
+                                                self.b_get_scan_region = true;
+                                            }
+                                            _ => {}
                                         }
-                                        _ => {}
                                     }
                                 }
+                                Err(_) => {}
                             }
-                            Err(_) => {}
                         }
                     }
 
                     if self.b_get_scan_region {
                         ui.horizontal(|ui| {
-                            if ui.button("attach rtt region").clicked() {
-                                match self
-                                    .probe_rs_handler
-                                    .attach_rtt_region(self.cur_target_core_idx)
-                                {
-                                    Ok(_) => {}
-                                    Err(_) => {}
+                            if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                                if ui.button("attach rtt region").clicked() {
+                                    match h.attach_rtt_region(self.cur_target_core_idx) {
+                                        Ok(_) => {}
+                                        Err(_) => {}
+                                    }
+                                    h.get_up_channels_size();
                                 }
-                                self.probe_rs_handler.get_up_channels_size();
-                            }
-                            ui.add(
-                                egui::Slider::new(&mut self.retry_rtt_attach_time_out, 0..=10000)
+                                ui.add(
+                                    egui::Slider::new(
+                                        &mut self.retry_rtt_attach_time_out,
+                                        0..=10000,
+                                    )
                                     .text("time out duration (ms)"),
-                            );
-                            if ui.button("try to attach rtt rigion with timeout").clicked() {
-                                let time_out_duration =
-                                    Duration::from_millis(self.retry_rtt_attach_time_out);
-                                match self
-                                    .probe_rs_handler
-                                    .attach_retry_loop(self.cur_target_core_idx, time_out_duration)
-                                {
-                                    Ok(_) => {}
-                                    Err(_) => {}
+                                );
+                                if ui.button("try to attach rtt rigion with timeout").clicked() {
+                                    let time_out_duration =
+                                        Duration::from_millis(self.retry_rtt_attach_time_out);
+                                    match h.attach_retry_loop(
+                                        self.cur_target_core_idx,
+                                        time_out_duration,
+                                    ) {
+                                        Ok(_) => {}
+                                        Err(_) => {}
+                                    }
+                                    h.get_up_channels_size();
                                 }
-                                self.probe_rs_handler.get_up_channels_size();
                             }
                         });
                     }
@@ -208,25 +234,26 @@ pub mod m_rtt_opts {
             ui.separator();
 
             ui.horizontal(|ui| {
-                if let Some(_rtt) = self.probe_rs_handler.rtt.borrow() {
-                    egui::ComboBox::from_label("channel")
-                        .selected_text(format!("{}", self.cur_target_channel_idx))
-                        .show_ui(ui, |ui| {
-                            for c in 0..self.probe_rs_handler.up_chs_size {
-                                ui.selectable_value(
-                                    &mut self.cur_target_channel_idx,
-                                    c,
-                                    format!("{}", c),
-                                );
-                            }
-                        });
+                if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                    if let Some(_rtt) = h.rtt.borrow() {
+                        egui::ComboBox::from_label("channel")
+                            .selected_text(format!("{}", self.cur_target_channel_idx))
+                            .show_ui(ui, |ui| {
+                                for c in 0..h.up_chs_size {
+                                    ui.selectable_value(
+                                        &mut self.cur_target_channel_idx,
+                                        c,
+                                        format!("{}", c),
+                                    );
+                                }
+                            });
 
-                    if ui.button("take channel").clicked() {
-                        self.probe_rs_handler
-                            .get_one_up_ch(self.cur_target_channel_idx);
+                        if ui.button("take channel").clicked() {
+                            h.get_one_up_ch(self.cur_target_channel_idx);
+                        }
+                        ui.add_space(4.0);
+                        ui.checkbox(&mut self.b_try_to_read, "try to read");
                     }
-                    ui.add_space(4.0);
-                    ui.checkbox(&mut self.b_try_to_read, "try to read");
                 }
             });
 
@@ -238,26 +265,25 @@ pub mod m_rtt_opts {
             let mut buf = [0u8; 128];
             let mut read_size = 0;
             if self.b_try_to_read {
-                match self
-                    .probe_rs_handler
-                    .rtt_read_from_channel(&mut buf, self.cur_target_channel_idx)
-                {
-                    Ok(s) => {
-                        read_size = s;
-                        if read_size > 0 {
-                            let local_date_time = Local::now();
-                            let ymdhms = local_date_time.format("%Y-%m-%d %H:%M:%S%.3f");
-                            let text = format!(
-                                "{}: {} {}",
-                                self.n_items,
-                                ymdhms,
-                                String::from_utf8_lossy(&buf)
-                            );
-                            self.log_buf.push_back(text);
+                if let Some(h) = self.probe_rs_handler.borrow_mut() {
+                    match h.rtt_read_from_channel(&mut buf, self.cur_target_channel_idx) {
+                        Ok(s) => {
+                            read_size = s;
+                            if read_size > 0 {
+                                let local_date_time = Local::now();
+                                let ymdhms = local_date_time.format("%Y-%m-%d %H:%M:%S%.3f");
+                                let text = format!(
+                                    "{}: {} {}",
+                                    self.n_items,
+                                    ymdhms,
+                                    String::from_utf8_lossy(&buf)
+                                );
+                                self.log_buf.push_back(text);
+                            }
                         }
-                    }
-                    Err(e) => {
-                        ui.label(format!("{:#?}", e));
+                        Err(e) => {
+                            ui.label(format!("{:#?}", e));
+                        }
                     }
                 }
             }
